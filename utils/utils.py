e3a175f9ce0b5d33359296a3af3b985723935877
Mengjielf <54342723+Mengjielf@users.noreply.github.com>
2022-11-03 21:02:16 +0800

    Add files via upload

--- utils/utils.py
+++ utils/utils.py
@@ -5,7 +5,7 @@ from . import common_utils
 import numpy as np
 import os
 
-def save_data(x,y,file_path):#保存训练、验证、测试数据
+def save_data(x,y,file_path):
     data=[]
     for i in range(len(x)):
         temp=[]
@@ -24,21 +24,19 @@ def save_final_data(reader, discretizer, normalizer, type,demographic_data,diagn
     data = ret["X"]
     ts = ret["t"]
     labels = ret["y"]
-    names = ret["name"]  #类似‘17774_episode1_timeseries.csv’
+    names = ret["name"]
     data_x = [discretizer.transform(X, end=t)[0] for (X, t) in zip(data, ts)]
     data=data_x
     if normalizer is not None:
         data = [normalizer.transform(X) for X in data]
-    #到这里就是完整的处理后的数据(shape=(48,76))及标签了   对于shape,所有的数据都是48*76,48是因为入院死亡率统计的是入院后48小时的情况，所以之前应该是做了一些处理，所以留下来的数据都是48h的
-    whole_data = (np.array(data), labels)
+        whole_data = (np.array(data), labels)
 
     labels = np.array(labels)
     labels = labels.reshape(len(labels), 1)
     static_data=[]
     for i in range(len(names)):
-        cur_id, cur_ep, _ = names[i].split('_', 2)  # batch_name[i]类似于“96468_episode1_timeseries.csv”
+        cur_id, cur_ep, _ = names[i].split('_', 2)
         cur_idx = cur_id + '_' + cur_ep
-        # print(cur_idx)  #类似’subjectid'+'_'+'episode'+num,如： 96468_episode1
         cur_demo = demographic_data[idx_list.index(cur_idx)]
         cur_diag=diagnosis_data[idx_list.index(cur_idx)]
         cur_static_data=np.hstack((cur_demo,cur_diag))
